91c95a3ad0221f010363544bafc544c908029059
chathurangasandun <csk@tiqri.com>
2019-06-24 13:05:37 +0300

    change

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-DOCKER_TAG=''
-
-case "$TRAVIS_BRANCH" in
-  "master")
-    DOCKER_TAG=latest
-    ;;
-  "develop")
-    DOCKER_TAG=dev
-    ;;    
-esac
-
+DOCKER_TAG='latest'
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t dshop.services.customers:$DOCKER_TAG .
 docker tag dshop.services.customers:$DOCKER_TAG $DOCKER_USERNAME/dshop.services.customers:$DOCKER_TAG
