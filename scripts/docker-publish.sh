777bd1469e6d1c67e80cfcfa5179b284035c4b61
chathurangasandun <csk@tiqri.com>
2019-07-03 12:25:42 +0300

    revert

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -2,6 +2,6 @@
 ls
 DOCKER_TAG='latest'
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -t accoon.pitstop.customermanagement:$DOCKER_TAG src/Pitshop.CustomerApi/Accoon.Pitshop.CustomerApi.Presenter/Dockerfile.prod
+docker build -t accoon.pitstop.customermanagement:$DOCKER_TAG src/Pitshop.CustomerApi/Accoon.Pitshop.CustomerApi.Presenter
 docker tag accoon.pitstop.customermanagement:$DOCKER_TAG $DOCKER_USERNAME/accoon.pitstop.customermanagement:$DOCKER_TAG
 docker push $DOCKER_USERNAME/accoon.pitstop.customermanagement:$DOCKER_TAG
